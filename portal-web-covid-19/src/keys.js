2eed6f211622c303be822bf444498b0b2efa37c4
SebastianAsis <sasisromero10@gmail.com>
2020-07-15 16:49:10 -0500

    Database connection created

---
+++ portal-web-covid-19/src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        //connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        //database: 'db_links'
+    }
+
+};
